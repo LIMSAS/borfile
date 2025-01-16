f04509c501bbfe6563f0ef71bb1f12903f4e170d
Salem Harrache <salem.harrache@lim.eu>
2025-01-16 19:57:07 +0100

    test that the metadata is not accessed if we only read the data_nc

--- tests/test_dataset.py
+++ tests/test_dataset.py
@@ -46,8 +46,13 @@ def test_compare_dataset_and_dataframe(bor_file):
 
 
 def test_compare_metadata(bor_file):
-    assert not hasattr(bor_file, "_dataframe")
+    assert not hasattr(bor_file, "_data")
     ds = open_dataset(bor_file.data_nc)
-    assert not hasattr(bor_file, "_dataframe")
+    assert not hasattr(bor_file, "_data")
+
+    metadata = bor_file.metadata
+    assert hasattr(bor_file, "_data")
+    assert hasattr(bor_file, "_metadata")
+
     for var in list(ds.variables):
-        ds[var].attrs == bor_file.metadata[var]
+        ds[var].attrs == metadata[var]
