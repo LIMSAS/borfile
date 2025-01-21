8a8825473d2a727bd3ddd4d0a6a8ca0236fc1150
Salem Harrache <salem.harrache@lim.eu>
2025-01-21 11:55:08 +0100

    assert_same_files function copies files if missing before assert

--- tests/utils.py
+++ tests/utils.py
@@ -9,8 +9,8 @@ def glob_bor_files(data_dir):
     return sorted(set(files), key=lambda x: x.name)
 
 
-def assert_same_files(f1, f2, force_copy_before=False):
-    if force_copy_before:
+def assert_same_files(f1, f2, copy_if_missing=True):
+    if copy_if_missing and not f2.exists():
         shutil.copy(f1, f2)
     fpath1, fpath2 = f1.as_posix(), f2.as_posix()
     assert filecmp.cmp(fpath1, fpath2), f"{fpath1} != {fpath2}"
