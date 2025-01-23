507d05031247f7eb825c41a1c83a80af7eaa96c8
Salem Harrache <salem.harrache@lim.eu>
2025-01-23 14:33:42 +0100

    add bor format codes

--- bor/__init__.py
+++ bor/__init__.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 # from .reader import *
 from .file import BorFile, read
+from . import codes
 
 __version__ = "0.1.0.dev"
 VERSION = __version__
--- tests/test_read_bor.py
+++ tests/test_read_bor.py
@@ -19,84 +19,6 @@ def bor_file(request):
         return bor.read(request.param)
 
 
-DRILLING_METHOD_CODES = (
-    "DRLMTD_HA",
-    "DRLMTD_CFA",
-    "DRLMTD_ADM",
-    "DRLMTD_HSA",
-    "DRLMTD_DTM",
-    "DRLMTD_COR",
-    "DRLMTD_RTR",
-    "DRLMTD_RRFFM",
-    "DRLMTD_RTRPRC",
-    "DRLMTD_RPM",
-    "DRLMTD_DTH",
-    "DRLMTD_DRI",
-    "DRLMTD_DS",
-    "DRLMTD_DST",
-    "DRLMTD_STDTM",
-    "DRLMTD_CPD",
-    "DRLMTD_VDS",
-    "DRLMTD_VD",
-    "DRLMTD_VS",
-    "DRLMTD_PS",
-)
-
-DRILLING_PARAMETERS_LOG = (
-    "time",
-    "DEPTH",
-    "AS",
-    "EVP",
-    "EVR",
-    "EVS",
-    "TP",
-    "IP",
-    "TQ",
-    "TQC",
-    "RSP",
-    "RSPC",
-    "HP",
-    "SP",
-    "RV",
-    "IF",
-    "OF",
-    "IV",
-    "OV",
-    "AP",
-    "AF",
-    "AV",
-    "WF",
-    "WP",
-    "WV",
-    "ECM",
-    "PHM",
-    "DO2M",
-    "TEMPM",
-)
-
-
-PRESSUREMETER_TEST_TYPES = ("ground", "volume_loss", "pressure_loss")
-
-PRESSUREMETER_LOG_NAMES = (
-    "time",
-    "STEP",
-    "PR1",
-    "PR15",
-    "PR30",
-    "PR60",
-    "PG1",
-    "PG15",
-    "PG30",
-    "PG60",
-    "V1",
-    "V15",
-    "V30",
-    "V60",
-    "CREEP",
-    "DELT60",
-)
-
-
 def test_description(bor_file):
     assert bor_file.description_xml != ""
     assert "borehole_ref" in bor_file.description
@@ -115,7 +37,9 @@ def test_description(bor_file):
         assert bor_file.description["convention"]["parameters"]["logfile"] == "data.nc"
         assert "drilling" in bor_file.description
         if "method" in bor_file.description["drilling"]:
-            assert bor_file.description["drilling"]["method"] in DRILLING_METHOD_CODES
+            assert (
+                bor_file.description["drilling"]["method"] in bor.codes.DRILLING_METHOD
+            )
 
     if bor_file.domain == "MENARD PRESSUREMETER TEST":
         assert bor_file.description["convention"]["@version"] in ("1.1", "1.2")
@@ -123,7 +47,7 @@ def test_description(bor_file):
         assert any(
             [
                 ptt in bor_file.description["convention"]["pressuremeter"]
-                for ptt in PRESSUREMETER_TEST_TYPES
+                for ptt in bor.codes.PRESSUREMETER_TEST_TYPES
             ]
         )
 
@@ -155,13 +79,13 @@ def test_dataframe_parameters(bor_file):
         assert "DEPTH" in bor_file.data
         assert "time" in bor_file.data.reset_index(drop=False)
         for parameter in list(bor_file.data.columns):
-            assert parameter in DRILLING_PARAMETERS_LOG
+            assert parameter in bor.codes.DRILLING_PARAMETERS_LOG
 
     if bor_file.domain == "MENARD PRESSUREMETER TEST":
         assert "STEP" in bor_file.data
         assert "time" not in bor_file.data
         for parameter in list(bor_file.data.columns):
-            assert parameter in PRESSUREMETER_LOG_NAMES
+            assert parameter in bor.codes.PRESSUREMETER_LOG_NAMES
 
 
 def test_dataset_parameters(bor_file):
@@ -169,12 +93,15 @@ def test_dataset_parameters(bor_file):
     assert ds.to_array().size > 0
     assert len(list(ds.variables)) > 0
 
+    assert "time" in ds.indexes
     if bor_file.domain == "DRILLING PARAMETERS":
-        assert "DEPTH" in ds.variables
-        for parameter in list(ds.variables):
-            assert parameter in DRILLING_PARAMETERS_LOG
+        assert "DEPTH" in ds.data_vars
+        assert "time" not in ds.data_vars
+        for parameter in list(ds.data_vars):
+            assert parameter in bor.codes.DRILLING_PARAMETERS_LOG
 
     if bor_file.domain == "MENARD PRESSUREMETER TEST":
-        assert "STEP" in ds.variables
-        for parameter in list(ds.variables):
-            assert parameter in PRESSUREMETER_LOG_NAMES
+        assert "STEP" in ds.data_vars
+        assert "time" not in ds.data_vars
+        for parameter in list(ds.data_vars):
+            assert parameter in bor.codes.PRESSUREMETER_LOG_NAMES
